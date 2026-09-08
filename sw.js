6b9cc8a922830b1e54f4e133b6d4e3324edb72ba
salman2623 <salu262394@gmail.com>
2026-09-08 23:50:11 +0530

    Update service worker cache version to v2

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
-const CACHE="leave-tracker-v1";const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="leave-tracker-v2";
+const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
